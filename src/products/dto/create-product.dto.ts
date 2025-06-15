8ad46f8487e6240008285d447638088ffb024c65
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-06-14 23:08:41 -0500

    Mejorando el CreateDto con validaciones

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,24 @@
+import { IsString, IsNumber, IsNotEmpty, IsPositive, Min, MaxLength } from 'class-validator';
+
 export class CreateProductDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(100)
     name: string;
+    
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(200)
     description: string;
+    
+    @IsNumber({ maxDecimalPlaces: 2 })
+    @IsPositive()
+    @Min(0)
     price: number;
+    
+    @IsNumber()
+    @IsPositive()
+    @Min(0)
     stock: number;
 }
