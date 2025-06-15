03ee2fb715dfd45de01b331bbf13fdfb6ce0b0f6
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-06-14 22:54:59 -0500

    Configurando TypeORM - Transformando el Entity de Products - Configurar el Modulo de Products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Importar TypeOrmModule para la conexión a la base de datos
+import { ConfigModule } from '@nestjs/config'; // Importar ConfigModule para variables de entorno
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Tipo de base de datos
+      host: process.env.DB_HOST, // Host de la base de datos
+      port: Number(process.env.DB_PORT), // Puerto por defecto de PostgreSQL
+      username: process.env.DB_USERNAME, // Nombre de usuario de la base de datos
+      password: process.env.DB_PASSWORD, // Contraseña de la base de datos
+      database: process.env.DB_NAME, // Nombre de la base de datos
+      autoLoadEntities: true, // Cargar automáticamente las entidades
+      synchronize: true, // Sincronizar la base de datos (no recomendado en producción)
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Importar TypeOrmModule
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { Product } from './entities/product.entity'; // Importar la entidad de Product
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])], // Importar la entidad Product para TypeORM
   controllers: [ProductsController],
   providers: [ProductsService],
 })
