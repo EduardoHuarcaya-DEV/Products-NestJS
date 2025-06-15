1b7be72e019f1f59b5ea72c6bff6440a3aa45514
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-06-14 23:15:54 -0500

    Mejora en el manejo de errores del servicio de Products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import { NotFoundException } from '@nestjs/common/exceptions/not-found.exception';
 
 @Injectable()
 export class ProductsService {
@@ -11,26 +12,36 @@ export class ProductsService {
     @InjectRepository(Product)
     private productRepository: Repository<Product>,
   ) {}
-  create(createProductDto: CreateProductDto) {
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
     const product = this.productRepository.create(createProductDto);
-    return this.productRepository.save(product);
+    return await this.productRepository.save(product);
   }
 
-  findAll() {
-    return this.productRepository.find();
+  async findAll(): Promise<Product[]> {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return this.productRepository.findOne({ where: { id } });
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productRepository.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException(`Producto with id ${id} not found`);
+    }
+    return product;
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
+  async update(
+    id: number,
+    updateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    const product = await this.findOne(id); // Lanazará el NotFoundException si no se encuentra el producto
     await this.productRepository.update(id, updateProductDto);
-    return this.findOne(id)
+    return this.findOne(id); // Retorna el producto actualizado
   }
 
-  async remove(id: number) {
+  async remove(id: number): Promise<{ message: string }> {
+    const product = await this.findOne(id); // Lanazará el NotFoundException si no se encuentra el producto
     await this.productRepository.delete(id);
-    return { message: `Product with id ${id} has been deleted` }
+    return { message: `Producto with id ${id} has been removed` };
   }
 }
